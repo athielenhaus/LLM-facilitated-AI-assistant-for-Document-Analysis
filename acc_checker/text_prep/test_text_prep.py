abde5a2d969ed9d9ad6cf39d810a9fb32f7d1e7a
Arne Thielenhaus <arne.thielenhaus@gmail.com>
2023-07-11 16:20:50 +0200

    test class update

--- acc_checker/text_prep/test_text_prep.py
+++ acc_checker/text_prep/test_text_prep.py
@@ -1,5 +1,6 @@
 import os
 from text_prep2 import FileProcessor
+from langchain.docstore.document import Document
 import unittest
 
 class TestFileProcessor(unittest.TestCase):
@@ -9,16 +10,42 @@ class TestFileProcessor(unittest.TestCase):
         fp = FileProcessor()
 
 
-    # def test_save_as_temp_file(self):   # can take file
-    #     fp = FileProcessor()
-    #     with open('test.pdf', 'rb') as file:
-    #         fp.save_as_temp_file(file)
-
-
     def test_load_data(self):
         fp = FileProcessor()
         data = fp.load_data('test.pdf')
-        assert 'Foo-bar ban-anas Py-Charm' in data.page_content
+        assert 'Foo-bar ban-anas Py-Charm' in data[0].page_content
+
+
+    def test_save_as_temp_file_and_get_data(self):   # can take file
+        fp = FileProcessor()
+        with open('test.pdf', 'rb') as file:
+            data = fp.save_as_temp_file_and_get_data(file)
+        assert 'Foo-bar ban-anas Py-Charm' in data[0].page_content
+
+    def test_clean_docs(self):
+        fp = FileProcessor()
+        doc1 = Document(page_content="\nsome sam-\nple text\n\nnew heading", metadata={"source": "local"})
+        doc2 = Document(page_content="\n\n\n\n\n\n\nsome more sam-\nple text\n\nnew heading\neven more text", metadata={"source": "local"})
+        test_docs= [doc1, doc2]
+        cleaned_docs = fp.clean_docs(test_docs)
+        assert cleaned_docs[0].page_content == " some sample text\n\nnew heading"
+        assert cleaned_docs[1].page_content == "\n\nsome more sample text\n\nnew heading even more text"
+
+    def test_get_full_text(self):
+        fp = FileProcessor()
+        doc1 = Document(page_content= "Hello ")
+        doc2 = Document(page_content="World")
+        full_text = fp.get_full_text([doc1, doc2])
+        assert full_text == 'Hello World'
+
+
+    def test_split_text_recursively(self):
+        text = "This is a simple random test string\n\n for checking recursive splitting. It should split on double new lines, new lines, end of sentences and finally spaces."
+        text_chunks = split_text_recursively(text, )
+        assert text_chunks[0] == "This is a simple random test string"
+
+
+
 
 
 if __name__ == '__main__':
--- acc_checker/text_prep/text_prep2.py
+++ acc_checker/text_prep/text_prep2.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tiktoken
 import tempfile
 import os
-import pdfplumber
+import re
 
 
 class FileProcessor:
@@ -21,23 +21,49 @@ class FileProcessor:
         self.token_price = 0
 
 
-    def save_as_temp_file(self, file_obj):
+    def save_as_temp_file_and_get_data(self, file_obj):
         with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as temp_file:
             temp_path = temp_file.name
 
             # Write the contents of the file-type object to the temporary file
             temp_file.write(file_obj.read())
+            data = self.load_data(temp_path)
 
+            temp_file.close()
             os.remove(temp_path)
 
+        return data
 
+
+    # PDFPlumberLoader.load() returns list with page content and metadata for each page in PDF
     def load_data(self, file_path):
         loader = PDFPlumberLoader(file_path)
         data = loader.load()
         return data
 
 
+    def clean_docs(self, data):
+        cleaned_docs = []
+        for doc in data:
+            text = doc.page_content
+            clean_content = re.sub(r'\n{2,}', '###!!!###', text).replace('-\n', '').replace('\n',' ').replace('###!!!###','\n\n')
+            cleaned_doc = Document(page_content=clean_content, metadata=doc.metadata)
+            cleaned_docs.append(cleaned_doc)
+        return cleaned_docs
 
-fp= FileProcessor()
-data = fp.load_data('test.pdf')
-print(data)
+    def get_full_text(self, docs):
+        full_text = ''
+        for doc in docs:
+            full_text += doc.page_content
+        return full_text
+
+    def get_text_chunks(self, full_text, chunk_size=1000, chunk_overlap=200):
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n", "(?<=\. )", " ", ""],
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+        )
+        text_chunks = text_splitter.create_documents([full_text])
+        return text_chunks
+
